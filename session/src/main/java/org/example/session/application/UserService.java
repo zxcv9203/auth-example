8310194536187777cfe03f4b2487fcc43bfafb15
yongckim <zxcv9203@naver.com>
2024-01-14 00:28:01 +0900

    :sparkles: 회원가입 기능 구현

--- session/src/main/java/org/example/session/application/UserService.java
+++ session/src/main/java/org/example/session/application/UserService.java
@@ -2,7 +2,10 @@ package org.example.session.application;
 
 import lombok.RequiredArgsConstructor;
 import org.example.session.domain.user.User;
+import org.example.session.domain.user.UserRole;
 import org.example.session.infrastructure.repository.UserRepository;
+import org.example.session.infrastructure.web.request.UserSignupRequest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,9 +13,22 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public User findByUsername(String username) {
         return userRepository.findByUsername(username)
                 .orElseThrow(() -> new IllegalArgumentException("찾을 수 없는 사용자 이름입니다. 전달 받은 사용자 이름 : " + username));
+    }
+
+    public void signup(UserSignupRequest request) {
+        String password = passwordEncoder.encode(request.password());
+
+        User user = User.builder()
+                .username(request.username())
+                .password(password)
+                .role(UserRole.MEMBER)
+                .build();
+
+        userRepository.save(user);
     }
 }
