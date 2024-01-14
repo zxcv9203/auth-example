dccb3cc3537534649435688121f5d2e38c69e1cc
yongckim <zxcv9203@naver.com>
2024-01-14 19:37:26 +0900

    :sparkles: 로그인 실패시 try-catch를 이용해 Exception Handling

--- session/src/main/java/org/example/session/infrastructure/security/LoginFilter.java
+++ session/src/main/java/org/example/session/infrastructure/security/LoginFilter.java
@@ -9,6 +9,7 @@ import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.session.infrastructure.security.request.LoginRequest;
+import org.example.session.infrastructure.security.response.LoginFailResponse;
 import org.example.session.infrastructure.security.response.LoginResponse;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -32,21 +33,30 @@ public class LoginFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         if (HttpMethod.POST.matches(request.getMethod()) && LOGIN_PATH.equals(request.getRequestURI())) {
-            LoginRequest loginRequest = objectMapper.readValue(request.getInputStream(), LoginRequest.class);
+            try {
+                LoginRequest loginRequest = objectMapper.readValue(request.getInputStream(), LoginRequest.class);
 
-            UsernamePasswordAuthenticationToken token = UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.username(), loginRequest.password());
-            Authentication authenticate = authenticationManager.authenticate(token);
+                UsernamePasswordAuthenticationToken token = UsernamePasswordAuthenticationToken.unauthenticated(loginRequest.username(), loginRequest.password());
+                Authentication authenticate = authenticationManager.authenticate(token);
 
-            SecurityContextHolder.getContext().setAuthentication(authenticate);
+                SecurityContextHolder.getContext().setAuthentication(authenticate);
 
-            HttpSession session = request.getSession(true);
-            session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
+                HttpSession session = request.getSession(true);
+                session.setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
 
-            response.setStatus(HttpServletResponse.SC_OK);
-            response.setCharacterEncoding("utf8");
-            response.setContentType("application/json");
-            response.getWriter().write(objectMapper.writeValueAsString(new LoginResponse("로그인에 성공했습니다.")));
-            return;
+                response.setStatus(HttpServletResponse.SC_OK);
+                response.setCharacterEncoding("utf8");
+                response.setContentType("application/json");
+                response.getWriter().write(objectMapper.writeValueAsString(new LoginResponse("로그인에 성공했습니다.")));
+                return;
+            } catch (RuntimeException e) {
+                log.warn(e.getMessage());
+                response.setContentType("application/json");
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                response.setCharacterEncoding("utf-8");
+                response.getWriter().write(objectMapper.writeValueAsString(new LoginFailResponse("인증에 실패했습니다. ID나 패스워드를 확인해주세요.")));
+                return;
+            }
         }
 
         filterChain.doFilter(request, response);
