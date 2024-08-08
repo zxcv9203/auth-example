c7e28f10ad115aa04c7ec3e27b65da295cf8cbc8
yongckim <zxcv9203@naver.com>
2024-08-08 23:34:23 +0900

    :tada: spring security with kotlin 초기화

---
+++ kotlin-spring-security/src/main/kotlin/org/example/kotlinspringsecurity/KotlinSpringSecurityApplication.kt
@@ -0,0 +1,11 @@
+package org.example.kotlinspringsecurity
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringSecurityApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringSecurityApplication>(*args)
+}
---
+++ kotlin-spring-security/src/test/kotlin/org/example/kotlinspringsecurity/KotlinSpringSecurityApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.kotlinspringsecurity
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringSecurityApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
